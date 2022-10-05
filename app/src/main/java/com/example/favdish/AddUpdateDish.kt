bb91190460658b064f8440545fc1a691788513ab
IamBansal <abansal11ae@gmail.com>
2022-10-05 20:43:53 +0530

    Camera Added

--- app/src/main/java/com/example/favdish/AddUpdateDish.kt
+++ app/src/main/java/com/example/favdish/AddUpdateDish.kt
@@ -1,12 +1,30 @@
 package com.example.favdish
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.favdish.databinding.ActivityAddUpdateDishBinding
 import com.example.favdish.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateDish : AppCompatActivity(), View.OnClickListener {
 
@@ -31,30 +49,117 @@ class AddUpdateDish : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun customImageSelectionDialog(){
+    private fun customImageSelectionDialog() {
         val dialog = Dialog(this)
         val dialogBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(dialogBinding.root)
 
         dialogBinding.camera.setOnClickListener {
-            Toast.makeText(this@AddUpdateDish, "Camera clicked", Toast.LENGTH_SHORT).show()
+
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
+
         dialogBinding.gallery.setOnClickListener {
-            Toast.makeText(this@AddUpdateDish, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    Toast.makeText(this@AddUpdateDish, "Gallery Allowed", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDish,
+                        "You have denied the permissions required.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+
+            }).onSameThread().check()
             dialog.dismiss()
         }
 
         dialog.show()
     }
 
+    private fun showDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions required for this feature.\nIt can be enabled under Application Settings.")
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
     override fun onClick(view: View?) {
         if (view != null) {
-            when(view.id) {
+            when (view.id) {
                 R.id.addDishImage -> {
                     customImageSelectionDialog()
                 }
             }
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.extras?.let {
+                    val thumbnail: Bitmap = it.get("data") as Bitmap
+                    binding.dishImage.setImageBitmap(thumbnail)
+                    binding.addDishImage.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this,
+                            R.drawable.ic_baseline_edit_24
+                        )
+                    )
+                }
+            }
+        }
+
+    }
+
+    companion object {
+        private const val CAMERA = 1
     }
 }
